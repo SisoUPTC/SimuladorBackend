900a10a21dcc5dca92c57b96206dee8d64900935
Cristian Parada <crispo9028@gmail.com>
2023-09-08 17:54:44 -0500

    First commit

---
+++ src/main/java/co/edu/uptc/so/simluador_backend/SimluadorBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.so.simluador_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimluadorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimluadorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uptc/so/simluador_backend/SimluadorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.so.simluador_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimluadorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
