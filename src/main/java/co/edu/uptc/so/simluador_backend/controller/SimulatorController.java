9faefa892d42c438a6038c05f95ba0b0790b4c36
Cristian Parada <crispo9028@gmail.com>
2023-09-17 10:48:52 -0500

    feature: Api general response

--- src/main/java/co/edu/uptc/so/simluador_backend/controller/SimulatorController.java
+++ src/main/java/co/edu/uptc/so/simluador_backend/controller/SimulatorController.java
@@ -3,31 +3,80 @@ package co.edu.uptc.so.simluador_backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
 import co.edu.uptc.so.simluador_backend.DTO.Data;
+import co.edu.uptc.so.simluador_backend.DTO.GraphicsDTO;
 import co.edu.uptc.so.simluador_backend.services.SimulatorService;
+import co.edu.uptc.so.simluador_backend.util.ApiRespone;
 
 @RestController
 @RequestMapping("/simulator")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SimulatorController {
 
     @Autowired
     SimulatorService simulatorService;
 
     @PostMapping("/start/{time}")
-    public ResponseEntity<String> start(@PathVariable int time) {
-        simulatorService.start(time);
-        return ResponseEntity.ok("Simulación creada exitosamente");
+    public ResponseEntity<ApiRespone> start(@PathVariable int time) {
+        try {
+            simulatorService.start(time);
+            ApiRespone apiRespone = new ApiRespone("simulacion creada exitosamente ", true, 200, "simulacion creada");
+            return new ResponseEntity<>(apiRespone, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            ApiRespone apiRespone = new ApiRespone("Error al crear la simulacion", false, 400, e);
+            return new ResponseEntity<>(apiRespone, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/results/{clock}")
-    public ResponseEntity<Data> results(@PathVariable int clock) {
-        Data result = simulatorService.getResult(clock);
-        return new ResponseEntity<>(result, HttpStatus.FOUND);
+    public ResponseEntity<ApiRespone> results(@PathVariable int clock) {
+        try {
+            Data result = simulatorService.getResult(clock);
+            ApiRespone apiRespone = new ApiRespone("resultado de la simulacion", true, 200, result);
+            return new ResponseEntity<>(apiRespone, HttpStatus.OK);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            ApiRespone apiRespone = new ApiRespone("Ha ocurrido un error al traer resultados de la simulacion", false,
+                    400, e);
+            return new ResponseEntity<>(apiRespone, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/graphics")
+    public ResponseEntity<ApiRespone> graphics() {
+        try {
+            GraphicsDTO result = simulatorService.getGraphics();
+            ApiRespone apiRespone = new ApiRespone("Graficas de la simulacion", true, 200, result);
+            return new ResponseEntity<>(apiRespone, HttpStatus.OK);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            ApiRespone apiRespone = new ApiRespone("Ha ocurrido un error al traer graficas de la simulacion", false,
+                    400, e);
+            return new ResponseEntity<>(apiRespone, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/isStarted")
+    public ResponseEntity<ApiRespone> isStarted(){
+        try {
+            boolean result = simulatorService.isStarted();
+            ApiRespone apiRespone = new ApiRespone("Estado de la simulacion", true, 200, result);
+            return new ResponseEntity<>(apiRespone, HttpStatus.OK);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            ApiRespone apiRespone = new ApiRespone("Ha ocurrido un error al traer el estado de la simulacion", false,
+                    400, e);
+            return new ResponseEntity<>(apiRespone, HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/co/edu/uptc/so/simluador_backend/services/SimulatorService.java
+++ src/main/java/co/edu/uptc/so/simluador_backend/services/SimulatorService.java
@@ -1,21 +1,32 @@
 package co.edu.uptc.so.simluador_backend.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import co.edu.uptc.so.simluador_backend.DTO.Data;
+import co.edu.uptc.so.simluador_backend.DTO.GraphicsDTO;
 import co.edu.uptc.so.simluador_backend.process_module.Simulator;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class SimulatorService {
+    @Autowired
     Simulator simulator;
 
-    public void start(int simulationTime) {
+    public boolean start(int simulationTime) {
         simulator.setSimulationTime(simulationTime);
-        simulator.start();
+        return simulator.start();
     }
 
     public Data getResult(int clock) {
        return simulator.getData().get(clock);
+    }
+
+    public GraphicsDTO getGraphics() {
+        return this.simulator.getGraphics();
+    }
+
+    public boolean isStarted() {
+        return !this.simulator.getData().isEmpty();
     }
 }
