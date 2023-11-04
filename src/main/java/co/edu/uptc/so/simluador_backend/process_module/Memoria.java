d7c35023c9135f6a9d0146116555ab9c0d6b9d6e
Cristian David Parada Martinez <crispo9028@gmail.com>
2023-11-04 18:33:30 -0500

    Create class memory

---
+++ src/main/java/co/edu/uptc/so/simluador_backend/process_module/Memoria.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.so.simluador_backend.process_module;
+
+import java.util.ArrayList;
+
+public class Memoria {
+    ArrayList<Process> processeList;
+    ArrayList<Process> inputList;
+    int sizeLimit;
+    int option = 1; // 1
+
+    // First-fit, best-fit, worst-fit y next fit.
+
+}
