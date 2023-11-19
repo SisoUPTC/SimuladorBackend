a0817f5d1ae27c694c30cff0868e3aca533a702d
BrayanCo <b.rayancol36@gmail.com>
2023-11-18 20:37:51 -0500

    ChangeNextFit

--- src/main/java/co/edu/uptc/so/simluador_backend/process_module/NextFitMemoryAllocator.java
+++ src/main/java/co/edu/uptc/so/simluador_backend/process_module/NextFitMemoryAllocator.java
@@ -16,21 +16,51 @@ public class NextFitMemoryAllocator {
     }
 
     public void allocateProcess(int processSize) {
-        Memory currentBlock = memory.get(nextFitPointer);
         int startIndex = nextFitPointer;
+        int remainingSize = processSize;
 
         do {
-            if (!currentBlock.allocated && currentBlock.sizeLimit >= processSize) {
+            Memory currentBlock = memory.get(nextFitPointer);
+
+            if (!currentBlock.allocated && currentBlock.sizeLimit >= remainingSize) {
                 // Allocate the process in the current block
                 currentBlock.allocated = true;
                 System.out.println("Allocated process of size " + processSize + " in block " + currentBlock.id);
-                return;
+                // Update the remaining size and reset the pointer
+                remainingSize = 0;
+                nextFitPointer = (nextFitPointer + 1) % memory.size();
+                break;
             }
+
             nextFitPointer = (nextFitPointer + 1) % memory.size();
-            currentBlock = memory.get(nextFitPointer);
         } while (nextFitPointer != startIndex);
 
-        System.out.println("Insufficient memory to allocate process of size " + processSize);
+        if (remainingSize > 0) {
+            System.out.println("Insufficient memory to allocate process of size " + processSize);
+        }
+
+        // Update the memory information
+        updateMemoryInfo();
+    }
+
+    private void updateMemoryInfo() {
+        // Merge adjacent unallocated blocks
+        List<Memory> mergedMemory = new ArrayList<>();
+        Memory currentBlock = memory.get(0);
+
+        for (int i = 1; i < memory.size(); i++) {
+            Memory nextBlock = memory.get(i);
+
+            if (!currentBlock.allocated && !nextBlock.allocated) {
+                currentBlock.sizeLimit += nextBlock.sizeLimit;
+            } else {
+                mergedMemory.add(currentBlock);
+                currentBlock = nextBlock;
+            }
+        }
+
+        mergedMemory.add(currentBlock);
+        memory = mergedMemory;
     }
 
     public void deallocateProcess(int processSize) {
@@ -38,6 +68,7 @@ public class NextFitMemoryAllocator {
             if (block.allocated && block.sizeLimit == processSize) {
                 block.allocated = false;
                 System.out.println("Deallocated process of size " + processSize + " from block " + block.id);
+                mergeAdjacentBlocks();
                 return;
             }
         }
@@ -50,18 +81,50 @@ public class NextFitMemoryAllocator {
             System.out.println("Block " + block.id + ": Size=" + block.sizeLimit + " Allocated=" + block.allocated);
         }
     }
+
+    public String getMemoryInfo() {
+        StringBuilder info = new StringBuilder("Memory Information:\n");
+        info.append("Process Queue:\n"); // Add information about the process queue if available
+        info.append("Occupied Memory: ").append(calculateOccupiedMemory()).append(" cells\n");
+        info.append("Free Memory: ").append(calculateFreeMemory()).append(" cells\n");
+        return info.toString();
+    }
+
+    private void mergeAdjacentBlocks() {
+        // Merge adjacent unallocated blocks
+        List<Memory> mergedMemory = new ArrayList<>();
+        Memory currentBlock = memory.get(0);
+        for (int i = 1; i < memory.size(); i++) {
+            Memory nextBlock = memory.get(i);
+            if (!currentBlock.allocated && !nextBlock.allocated) {
+                currentBlock.sizeLimit += nextBlock.sizeLimit;
+            } else {
+                mergedMemory.add(currentBlock);
+                currentBlock = nextBlock;
+            }
+        }
+        mergedMemory.add(currentBlock);
+        memory = mergedMemory;
+    }
+
+    private int calculateOccupiedMemory() {
+        int occupiedMemory = 0;
+        for (Memory block : memory) {
+            if (block.allocated) {
+                occupiedMemory += block.sizeLimit;
+            }
+        }
+        return occupiedMemory;
+    }
+
+    private int calculateFreeMemory() {
+        int freeMemory = 0;
+        for (Memory block : memory) {
+            if (!block.allocated) {
+                freeMemory += block.sizeLimit;
+            }
+        }
+        return freeMemory;
+    }
 }
 
-// public class NextFitMemoryAllocation {
-// public static void main(String[] args) {
-// NextFitMemoryAllocator memoryAllocator = new NextFitMemoryAllocator(100);
-
-// memoryAllocator.allocateProcess(20);
-// memoryAllocator.allocateProcess(40);
-// memoryAllocator.allocateProcess(10);
-// memoryAllocator.printMemoryStatus();
-
-// memoryAllocator.deallocateProcess(40);
-// memoryAllocator.printMemoryStatus();
-// }
-// }
